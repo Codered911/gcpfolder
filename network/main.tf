7fe51cb21244075298f37c2110d8a3083f7eab99
Codered911 <ajithpoojary10>
2025-12-11 12:34:15 +0000

    adding subnet

--- network/main.tf
+++ network/main.tf
@@ -4,3 +4,16 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = true
   mtu                     = 1460
 }
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  project = "apoojary"
+  network       = google_compute_network.vpc_network.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+depends_on = [ google_compute_network.vpc_network ]
+}
